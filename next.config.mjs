a7caa70c8ae31594a3ecb08ef768c21ec5d59509
LeoDuong28 <duongtrongnghia287@gmail.com>
2026-01-12 14:04:30 -0800

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,28 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: '**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
