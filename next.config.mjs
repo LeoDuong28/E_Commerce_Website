34d2d13455d6e8eb4a152c0017d6cff241c11ae9
LeoDuong28 <duongtrongnghia287@gmail.com>
2026-01-12 14:08:10 -0800

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -19,9 +13,6 @@ const nextConfig = {
         pathname: "**",
       },
     ],
-  },
-  turbopack: {
-    root: __dirname,
   },
 };
 
